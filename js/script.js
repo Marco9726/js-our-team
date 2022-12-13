d2b0275adc89ee758a8b5d74bcd7f5d9a8d72aa0
Marco 9726 <marcosarra9@gmail.com>
2022-12-13 16:19:10 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//1 creo array con i membri del team
+const team = [
+    //1.1 creo 6 oggetti dei membri del team all'interno dell'array
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    //1.1
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    //1.1
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    //1.1
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    //1.1
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    //1.1
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    }
+
+]
